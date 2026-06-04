b18673f5c05e58914e4e4e4b8be362a552045b67
Guentalina Allegretti <guentalina.allegretti@gmail.com>
2026-06-04 22:41:03 +0200

    fix cloudinary_storage position in INSTALLED_APPS

--- backend/build.sh
+++ backend/build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 cd api
-python manage.py collectstatic --no-input
-echo "==> Static files in staticfiles/: $(find staticfiles -type f | wc -l) files"
+python manage.py collectstatic --no-input --clear --verbosity 1
+echo "==> Static files collected: $(find staticfiles -type f 2>/dev/null | wc -l) files"
 python manage.py migrate
