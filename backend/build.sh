ca3b3bced39ef606c698785f3422ad32b85fb6ba
Guentalina Allegretti <guentalina.allegretti@gmail.com>
2026-06-04 22:38:47 +0200

    serve static files via Django serve view, add collectstatic debug output

--- backend/build.sh
+++ backend/build.sh
@@ -4,4 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 cd api
 python manage.py collectstatic --no-input
+echo "==> Static files in staticfiles/: $(find staticfiles -type f | wc -l) files"
 python manage.py migrate
